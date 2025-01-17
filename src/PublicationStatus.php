f8ada7fe2777af9e146ec7d539addc14c11b1d0c
SvenZimmo <sven@zimmo.be>
2025-01-17 10:31:26 +0100

    Update PublicationStatus.php

--- src/PublicationStatus.php
+++ src/PublicationStatus.php
@@ -6,7 +6,10 @@ namespace MaxImmo\ExternalParties;
 
 interface PublicationStatus
 {
-    public const CREATED = 'CREATED';
-    public const UPDATED = 'UPDATED';
-    public const REMOVED = 'REMOVED';
+    public const CREATED     = 'CREATED';
+    public const UPDATED     = 'UPDATED';
+    public const REMOVED     = 'REMOVED';
+    public const NOT_CREATED = 'NOT_CREATED';
+    public const NOT_UPDATED = 'NOT_UPDATED';
+    public const NOT_REMOVED = 'NOT_REMOVED';
 }
